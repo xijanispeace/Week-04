d75d31b4a2362c43f049f2726bd4820e38367040
Toeyty <61030236@kmitl.ac.th>
2019-09-20 20:44:24 +0700

    commit

--- 61030236/lab01/lab01/Program.cs
+++ 61030236/lab01/lab01/Program.cs
@@ -6,7 +6,15 @@ namespace lab01
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
+            for (float i = 0; i < Math.PI * 2.0F; i += 0.3F)
+            {
+                Console.WriteLine("The sine of {0,10:F} = {1,-10:F6}" + spaces(Math.Cos(i)) + "*", i, Math.Cos(i));
+            }
+        }
+        private static string spaces(double val)
+        {
+            string SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
+            return SpaceString;
         }
     }
 }
